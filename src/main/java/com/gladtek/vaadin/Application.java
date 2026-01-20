c6e25a51a8bb8703e304dc0398174b4344b6a3e9
Ahmed Chaabni <chaabni.dev@gmail.com>
2026-01-20 10:41:08 +0100

    feat: Open Graph meta tag injection.

--- src/main/java/com/gladtek/vaadin/Application.java
+++ src/main/java/com/gladtek/vaadin/Application.java
@@ -3,7 +3,7 @@ package com.gladtek.vaadin;
 
 import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.component.page.Meta;
+
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.aura.Aura;
 import org.springframework.boot.SpringApplication;
@@ -13,11 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @StyleSheet(Aura.STYLESHEET)
 @StyleSheet("styles.css")
 @PWA(name = "Star Wars Demo", shortName = "Star Wars")
-@Meta(name = "og:title", content = "Vaadin 25 Star Wars Demo")
-@Meta(name = "og:description", content = "A modern small demo application built with Vaadin 25 and Spring Boot, demonstrating advanced UI concepts, dynamic scheming dark/light mode, and full internationalization within a Star Wars-themed context.")
-@Meta(name = "og:image", content = "https://starwars.gladtek.com/icons/icon.png")
-@Meta(name = "og:url", content = "https://starwars.gladtek.com")
-@Meta(name = "og:type", content = "website")
+
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
